e2ecf16f1162f93e3cacebb248dd790822a42b3e
Souloinc87 <souloinc87@gmail.com>
2018-01-02 17:22:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens = [...kittens, "Ralph"]
+  return [...kittens, "Ralph"]
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob')
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop('Garfield')
+  return kittens
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift('Milo')
+  return kittens
+}
+function appendKitten(name) {
+  return [...kittens, 'Broom']
+}
